8950e246cd3e8c9b7709b78ab75986d0e7865315
Benjamin Torfu <btorfu@proton.me>
2024-07-26 03:54:00 +0000

    triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,11 @@
+#!/usr/bin/python3
+
 def pascal_triangle(n):
+    '''
+    returns a list of lists of integers representing the Pascal’s triangle of n
+
+    Returns an empty list if n <= 0
+    '''
     if n <= 0:
         return []
 
