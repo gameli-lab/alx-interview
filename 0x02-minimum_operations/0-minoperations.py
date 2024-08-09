da1507ede658665c90bfe9ff7f381f2edcd8dc62
Benjamin Torfu <btorfu@proton.me>
2024-08-09 05:56:48 +0000

    min num of operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,7 +5,7 @@ This module calculates the minimum number of times to copy H
 
 
 def minOperations(n: int) -> int:
-    
+
     '''
         minOperations finds the min number of operations to copy n number of H
         arg: n
@@ -18,7 +18,7 @@ def minOperations(n: int) -> int:
     factor = 2
 
     while n > 1:
-        while n % factor ==0:
+        while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
