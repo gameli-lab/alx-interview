6baba7663316809c343d59ace455176c9dc7ce31
Benjamin Torfu <btorfu@proton.me>
2024-09-14 22:01:51 +0000

    matrix rotation

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+''' A 2d matrix rotation module'''
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+
+    ''' Step 1: Transpose the matrix '''
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    ''' step 2: Reverse the matrix'''
+    for i in range(n):
+        matrix[i].reverse()
